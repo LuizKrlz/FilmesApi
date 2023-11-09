e84d6ea4983e0d6de4377bff32e27a9d1742a816
Luiz Carlos Rocha Correa <luiz.correa.dev@gmail.com>
2023-11-09 09:01:41 -0300

    Create endereco

---
+++ Data/Dtos/Endereco/ReadEnderecoDto.cs
@@ -0,0 +1,8 @@
+namespace FilmesApi.Data.Dtos.Endereco;
+
+public class ReadEnderecoDto
+{
+    public int Id { get; set; }
+    public string Logradouro { get; set; }
+    public int Numero { get; set; }
+}
---
+++ Data/Dtos/Endereco/UpdateEnderecoDto.cs
@@ -0,0 +1,7 @@
+namespace FilmesApi.Data.Dtos.Endereco;
+
+public class UpdateEnderecoDto
+{
+    public string Logradouro { get; set; }
+    public int Numero { get; set; }
+}
